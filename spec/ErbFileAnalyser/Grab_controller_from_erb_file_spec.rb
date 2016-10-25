2d50368c448193074ad0906c9b981c0fbb6022f1
jpms2 <jpms2@cin.ufpe.br>
2016-10-25 11:37:27 -0300

    Removing prints from code

--- spec/ErbFileAnalyser/Grab_controller_from_erb_file_spec.rb
+++ spec/ErbFileAnalyser/Grab_controller_from_erb_file_spec.rb
@@ -6,7 +6,6 @@ describe "Extraction of dependencies in erb files, including calls to controller
   it "does produce correct mappings for the files in the samples directory" do
     path = get_relative_path
     i = 1
-    print path
     $passed = true
     file_manager = File_manager.new
     while i < 5 do
@@ -16,7 +15,6 @@ describe "Extraction of dependencies in erb files, including calls to controller
       answer_string = file_manager.read_file("#{path}/samples/sample#{i}_expected_answer.txt").gsub(/\s+/, "")
       tool_answer = file_manager.read_file("#{path}/outputs/#{file_manager.get_file_name(sample_path)}_output.txt").gsub(/\s+/, "")
       i += 1
-      puts answer_string.length, tool_answer.length
       if !(answer_string == tool_answer)
         $passed = false
       end
