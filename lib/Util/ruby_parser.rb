2168f29e8ef8f7a0179b0ba78de1fa135c43b1cc
jpms2 <jpms2@cin.ufpe.br>
2016-11-11 14:03:51 -0300

    parser now really catches exceptions

--- lib/Util/ruby_parser.rb
+++ lib/Util/ruby_parser.rb
@@ -7,8 +7,10 @@ class Ruby_parser
 
   Parser::Builders::Default.emit_lambda = true # opt-in to most recent AST format
   def parse_code(code)
-    raise p Parser::CurrentRuby.parse(code)
+    begin
+      p Parser::CurrentRuby.parse(code)
     rescue Exception => e
       e.message
+    end
   end
 end
